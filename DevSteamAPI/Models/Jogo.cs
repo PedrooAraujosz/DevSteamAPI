813cbba620f139a63b5ef572945413c214c5415f
PEDRO ARAUJO <mxpedro.araujo11@gmail.com>
2025-02-05 16:37:57 -0300

    inclusão do authorize e AllowAnonymous

---
+++ DevSteamAPI/Models/Jogo.cs
@@ -0,0 +1,13 @@
+﻿namespace DevSteamAPI.Models
+{
+    public class Jogo
+    {
+        public Guid JogoId { get; set; }
+        public string Nome { get; set; }
+        public int classificacao { get; set; }
+        public string? Descricao { get; set; }
+        public string? imagem { get; set; }
+        public Guid CategoriaId { get; set; }
+        public Categoria? categoria { get; set; }
+    }
+}
